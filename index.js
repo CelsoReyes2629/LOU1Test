821e6eb7fc7021554d1aab0e6e186bf28579edb6
CelsoReyes2629 <celso.reyesu@uttn.mx>
2022-02-03 13:44:36 -0600

    First Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var products = [
+    {
+        name: "Pepsi 600 ml",
+        price: 17,
+        expirationDate: {
+          year: 2022,
+          month: "February",
+          day: 25
+        },
+        Brand: "Pepsi"
+      }, 
+      {
+        name: "Coca Cola 600 ml",
+        price: 18,
+        expirationDate: {
+          year: 2022,
+          month: "March",
+          day: 20
+        },
+        Brand: "Coca Cola"
+        },
+        {
+            name: "Big Cola 600 ml",
+            price: 14,
+            expirationDate: {
+              year: 2022,
+              month: "April",
+              day: 22
+            },
+            Brand: "Big Cola"
+        }, 
+        {
+            name: "Red Cola 600ml",
+            price: 12,
+            expirationDate: {
+              year: 2022,
+              month: "August",
+              day: 30
+            },
+            Brand: "Red Cola"
+        }
+      ]
+    
+    var table = document.getElementsByTagName("table")[0];
+    for (var i = 0; i < products.length; i++) {
+      var templ = document.getElementById("tbodytempl").content.cloneNode(true);
+      var tdnumber = templ.querySelector("span[name='tbodynumber']");
+      var tdname = templ.querySelector("span[name='tbodyname']");
+      var tdprice = templ.querySelector("span[name='tbodyprice']");
+      var tdbrand = templ.querySelector("span[name='tbodybrand']");
+    
+      tdnumber.innerText = i + 1;
+      tdname.innerText = products[i].Brand + " " + products[i].name;
+      tdprice.innerText = products[i].price;
+      tdbrand.innerText = products[i].Brand;
+    
+      table.append(templ);
+    }
